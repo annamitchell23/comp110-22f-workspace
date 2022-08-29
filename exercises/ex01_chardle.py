73180585582938eaa7d36f0f85fac7e9eacecdbb
Anna <amitchell@unc.edu>
2022-08-29 14:48:28 -0400

    Finished EX01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,18 +1,19 @@
 """EX01- Chardle- A cute step toward Wordle."""
 
-_author_ = "730477270"
+__author__ = "730477270"
 
 chosen_word: str = input("Enter a 5-chatacter word: ")
-if len(chosen_word)!= 5:
+if len(chosen_word) != 5:
     print("Error: Word must contain 5 characters")
     exit()
 else:
     chosen_character: str = input("Enter a single character: ")
-    if len(chosen_character)!= 1:
+    if len(chosen_character) != 1:
         print("Error: Character must be a single character")
         quit()
-    else: print("Searching for " + chosen_character + " in " + chosen_word)
-instance_counter: str = 0
+    else: 
+        print("Searching for " + chosen_character + " in " + chosen_word)
+instance_counter: int = 0
 if chosen_character == chosen_word[0]:
     print(chosen_character + " found at index 0 ")
     instance_counter += 1
@@ -34,11 +35,10 @@ if chosen_character == chosen_word[3]:
 else:
     pass
 if chosen_character == chosen_word[4]:
-     print(chosen_character + " found at index 4 ")
-     instance_counter += 1
+    print(chosen_character + " found at index 4 ")
+    instance_counter += 1
 else:
     pass
-
 
 if instance_counter == 0:
     print("No instances of " + chosen_character + " found in " + chosen_word)
