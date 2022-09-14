ff34adfe739db52b5fe8cf8e3ada251c60d6e6e8
Anna <amitchell@unc.edu>
2022-09-13 22:40:26 -0400

    Second progress EX03

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,9 +1,9 @@
 """Structured Wordle- getting even closer to the real thing!"""
 
-___author___= 730477270
+___author___ = 730477270
 
 def contains_char(secret_word: str, char: chr) -> bool:
-    """Function that scans the secret word for the presence of the guessed character"""
+    """Function that scans the secret word for the presence of the guessed character. """
     assert len(char) == 1
     index_counter: int = 0
     char_indicator: bool = False
@@ -18,12 +18,14 @@ def contains_char(secret_word: str, char: chr) -> bool:
     else:
         return False
 
+
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
+
 def emojified(guess_word: str, secret_word: str) -> str:
-    """Returns emoji chain based on the result of contains_char"""
+    """Returns emoji chain based on the result of contains_char."""
     assert len(guess_word) == len(secret_word)
     result_emoji: str = ""
     index_counter2: int = 0
@@ -39,15 +41,16 @@ def emojified(guess_word: str, secret_word: str) -> str:
         index_counter2 = index_counter2 + 1
     return result_emoji
 
+
 def input_guess(expected_length: int) -> str:
-    """Obtains guess of user (which is "expected length" long). If not expected length, prompts
-    user for another guess. If expected length, returns guess of correct length"""
+    """Obtains guess of user (which is "expected length" long). If not expected length, prompts user for another guess. If expected length, returns guess of correct length."""
     guess: str = input(f"Enter a { expected_length } character word: ")
     counter: int = 0
     while len(guess) != expected_length and counter < expected_length:
         guess = input(f"That wasn't { expected_length } chars! Try again: ")
         counter = counter + 1
     return guess
+
 
 def main() -> None:
     """The entrypoint of the program and main game loop."""
@@ -60,13 +63,14 @@ def main() -> None:
         # while the user still has turns left and they have not yet won
         print(f"=== Turn { turn }/6 ===")
         guess = (input_guess(length))
-        print(emojified(guess, secret_word))
+        print(emojified(guess, secret_word) ) 
         if guess == secret_word:
             won = True
             print(f"You won in { turn}/6 turns!" )
         if turn == 6 and secret_word != guess:
             print("X/6 - Sorry, try again tomorrow!" )
         turn = turn + 1
+
 
 if __name__ == "__main__":
     main()
